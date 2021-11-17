cc735583e948bfa3c037401224574baf9973ffb6
LynAudubon <lyn_audubon07@yahoo.com>
2021-11-17 11:24:12 -0800

    sample svelte app/Lynda

---
+++ src/components/Button.svelte
@@ -0,0 +1,44 @@
+<script>
+    export let style = 'primary'
+    export let type = 'button'
+    export let disabled = 'false'
+</script>
+
+<button {type} {disabled} class={style}>
+    <slot></slot>
+</button>
+
+<style>
+    button {
+       color: #fff;
+       border: 0;
+       border-radius: 8px;
+       color: #fff;
+       width: 100px; 
+    }
+
+    .primary {
+        background-color: #01AFD1; 
+        height: 35px;
+        width: 70px;
+    }
+
+    .secondary {
+        background-color: #F8E71C; 
+    }
+
+    button:hover {
+        transform: scale(0.98);
+        opacity: 0.9;
+    }
+
+    button:disabled {
+        background-color: #cccccc;
+        color: #333;
+    }
+
+    button:disabled:hover {
+        transform: scale(1);
+        opacity: 1;
+    }
+</style>
---
+++ src/components/Card.svelte
@@ -0,0 +1,15 @@
+<script></script>
+<div class='card'>
+    <slot></slot>
+</div>
+
+<style>
+    .card {
+        background-color: #fff;
+        color: #333;
+        border-radius: 12px;
+        padding: 40px, 50px;
+        margin: 30px 0px;
+        position: relative;
+    }
+</style>
